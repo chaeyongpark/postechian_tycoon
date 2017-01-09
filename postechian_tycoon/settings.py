9639c145e8e676279c8e4f1c8c143b7b003eb21d
cyPark <pcy8201@gmail.com>
2017-01-09 13:36:00 +0900

    hihi

--- postechian_tycoon/settings.py
+++ postechian_tycoon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'tycoon.apps.TycoonConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ tycoon/models.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class User(models.Model):
+	#user_id = models.CharField(max_length=20)
+	user_password = models.CharField(max_length=20)
+	is_pw_changed = models.BooleanField(default=False)
+
+class Title(models.Model):
+	name = models.CharField(max_length=20)
+
+class Item(models.Model):
+	name = models.CharField(max_length=20)
+	icon = models.ImageField(upload_to="images/icon") 
+	strength = models.IntegerField(default=0)
+	intelligence = models.IntegerField(default=0)
+	charm = models.IntegerField(default=0)
+	surplus = models.IntegerField(default=0)
+	luck = models.IntegerField(default=0)
+	explanation = models.CharField()
+
+class CodeToItem(models.Model):
+	code = models.CharField(max_length=20)
+	item = models.ForeignKey(Item)
+
+class Combination(models.Model):
+	item1 = models.ForeignKey(Item)
+	item2 = models.ForeignKey(Item)
+	new_item = models.ForeignKey(Item) 
+
+class Avatar(models.Model):
+	image = models.ImageField(upload_to="images/avatar")
+	strength = models.IntegerField(default=0)
+	intelligence = models.IntegerField(default=0)
+	charm = models.IntegerField(default=0)
+	surplus = models.IntegerField(default=0)
+	luck = models.IntegerField(default=0)
+	cur_title = models.ForeignKey(Title)
