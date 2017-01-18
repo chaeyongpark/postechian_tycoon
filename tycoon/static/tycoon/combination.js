5a2eb74a3955eaab25901c88d103256f6ec4deae
Lim Beom-Su <lbs1163@postech.ac.kr>
2017-01-18 12:06:21 +0900

    add AJAX prototype to combination

--- tycoon/static/tycoon/combination.js
+++ tycoon/static/tycoon/combination.js
@@ -1,3 +1,33 @@
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
 var left = false, right = false;
 
 var itemEventHandler = function(e) {
@@ -12,6 +42,18 @@ var itemEventHandler = function(e) {
 		$(this).parent().remove();
 		$("#item-right img").replaceWith(temp);
 		right = true;
+	}
+
+	if (left && right) {
+		var left_contains = $("#item-left").children().first().attr("contains");
+		var right_contains = $("#item-right").children().first().attr("contains");
+		$.ajax({
+			method: "POST",
+			url: "/combination",
+			data: { left: left_contains, right: right_contains }
+		}).done(function(data) {
+			alert(data.foo);
+		});
 	}
 }
 
--- tycoon/views.py
+++ tycoon/views.py
@@ -1,12 +1,15 @@
 #-*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Item, Avatar, Contain
 
 def home(request):
 	return render(request, 'tycoon/home.html')
 
 def combination(request):
-	avatar = Avatar.objects.get(pk=1)
-	class1_list = Contain.objects.filter(name__name__startswith='chaeyong').order_by('item')
-	return render(request, 'tycoon/combination.html', {'avatar': avatar, 'clist': class1_list})
+	if request.method == 'GET':
+		avatar = Avatar.objects.get(pk=1)
+		class1_list = Contain.objects.filter(name__name__startswith='chaeyong').order_by('item')
+		return render(request, 'tycoon/combination.html', {'avatar': avatar, 'clist': class1_list})
+	elif request.method == 'POST':
+		return JsonResponse({'foo': 'bar'})
