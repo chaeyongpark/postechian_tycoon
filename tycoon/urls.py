c21a787adf12fc36476f2e137c91358570ca47e9
Chaeyong Park <pcy8201@postech.ac.kr>
2017-01-21 13:05:09 +0900

    add form

--- tycoon/urls.py
+++ tycoon/urls.py
@@ -6,8 +6,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^avatar', views.avatar, name='avatar'),
-	url(r'^combination', views.combination, name='combination'),
 	url(r'^codeToItem', views.codeToItem, name='codeToItem'),
+	url(r'^use', views.use, name='use'),
+	url(r'^combination', views.combination, name='combination'),
 	url(r'^itemBook', views.itemBook, name='itemBook'),
 	url(r'^mission', views.mission, name='mission'),
 	url(r'^$', views.home, name='home'),
--- tycoon/views.py
+++ tycoon/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Item, Avatar, Contain, Combination
-from django.core import serializers
+from django.views.decorators.csrf import csrf_protect
 
 def home(request):
 	return render(request, 'tycoon/home.html')
@@ -21,15 +21,15 @@ def combination(request):
 		
 		try:
 			nitem = Combination.objects.get(item1__name=litem.item.name, item2__name=ritem.item.name)
-			print nitem.new_item
-			print nitem.new_item.icon
 			combined = {'id': Item.objects.get(name=nitem.new_item.name).id, 'url': nitem.new_item.icon.url}
-
 		except :
-			combined = {'id': 0, 'url': 'null'}
+			try:
+				nitem2 = Combination.objects.get(item2__name=litem.item.name, item1__name=ritem.item.name)
+				combined = {'id': Item.objects.get(name=nitem2.new_item.name).id, 'url': nitem2.new_item.icon.url}
+			except:
+				combined = {'id': 0, 'url': 'null'}
 
 		finally:
-			#res = serializers.serialize('json', combined)
 			return JsonResponse({'nitem': combined})
 
 def avatar(request):
@@ -37,11 +37,22 @@ def avatar(request):
 	return render(request, 'tycoon/avatar.html', {'avatar': avatar})
 
 def codeToItem(request):
-	return render(request, 'tycoon/codeToItem.html')
+	if request.method == 'GET':
+		print request.method
+		return render(request, 'tycoon/codeToItem.html')
+	
+	elif request.method == 'POST':
+		print request.POST
+	 	return render(request, 'tycoon/codeToItem.html')	
 
 def mission(request):
 	return render(request, 'tycoon/mission.html')
 
 def itemBook(request):
 	return render(request, 'tycoon/itemBook.html')
-	
+
+def use(request):
+	avatar = Avatar.objects.get(pk=1)
+	class1_list = Contain.objects.filter(name__name__startswith='chaeyong').order_by('item')
+	return render(request, 'tycoon/use.html', {'avatar': avatar, 'clist': class1_list})
+
