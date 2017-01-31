4c5a3599a0ab8d24199d6f951766e56eecc1234c
Chaeyong Park <pcy8201@postech.ac.kr>
2017-02-01 05:14:11 +0900

    GIMOREA

--- tycoon/context_processor.py
+++ tycoon/context_processor.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
+from .models import Avatar
 
 def pass_nav_all_users(request):
 	user = request.user.id
 	users = User.objects.filter(username__startswith='class')
-	return { "login_user": user, "all_users": users }
+	users_id = [ users[i].id for i in range(len(users))]
+	avatars = [ Avatar.objects.get(host=users[i].id).name for i in range(len(users))]
+	all_users = zip(users_id, avatars)
+	return { "login_user": user, "all_users": all_users }
